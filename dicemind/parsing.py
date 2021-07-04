d158585a27774135e4bceccfb098840b23a3044f
Kittenlover <artur.roos@mail.ru>
2021-07-05 00:12:18 +0300

    Ignore unary +

--- dicemind/parsing.py
+++ dicemind/parsing.py
@@ -17,9 +17,7 @@ class UnaryOptimizer(Transformer):
         return Tree("neg", tokens, None)
 
     def pos(self, tokens):
-        if tokens[0].data == "pos":
-            return tokens[0].children[0]
-        return Tree("pos", tokens, None)
+        return tokens[0]
 
 
 UNARY_OPTIMIZER = UnaryOptimizer(visit_tokens=True)
