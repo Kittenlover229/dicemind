20e4530f88480f8b5865fe91aa36df2386b57577
Kittenlover <artur.roos@mail.ru>
2021-07-04 23:26:55 +0300

    Inject dice values into metadata

--- dicemind/__main__.py
+++ dicemind/__main__.py
@@ -2,4 +2,5 @@ from .parsing import parse, optimize
 
 if __name__ == "__main__":
     while query := input("> "):
-        print(optimize(parse(query)).pretty())
+        optimal = optimize(parse(query))
+        print(optimal.pretty())
--- dicemind/parsing.py
+++ dicemind/parsing.py
@@ -1,4 +1,5 @@
-from lark import Tree, Lark, Transformer, Token
+from lark import Tree, Lark, Transformer
+from lark.tree import Meta
 from os import path
 
 parser = Lark(
@@ -24,9 +25,36 @@ class UnaryOptimizer(Transformer):
 UNARY_OPTIMIZER = UnaryOptimizer(visit_tokens=True)
 
 
+class DiceFieldInjector(Transformer):
+    def dice(self, children):
+        metadata = Meta()
+        metadata.empty = False
+
+        assert len(children) == 2
+        assert children[0].data == "dice_amount"
+        assert children[1].data == "dice_power"
+
+        if children[0].children:
+            metadata.amount = int(children[0].children[0].value)
+        else:
+            metadata.amount = 1
+
+        if children[1].children:
+            metadata.power = int(children[1].children[0].value)
+        else:
+            metadata.power = 20
+
+        return Tree("dice", [], metadata)
+
+
+DICE_FIELD_INJECTOR = DiceFieldInjector()
+
+OPTIMIZER = UNARY_OPTIMIZER * DICE_FIELD_INJECTOR
+
+
 def parse(string: str) -> Tree:
     return parser.parse(string)
 
 
 def optimize(tree: Tree) -> Tree:
-    return UNARY_OPTIMIZER.transform(tree)
+    return OPTIMIZER.transform(tree)
